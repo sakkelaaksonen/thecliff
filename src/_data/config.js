fd2893ff0a60451cb1eadd0f43aa3556616dfd3d
Sakari Laaksonen <sakke.laaksonen@gmail.com>
2025-05-31 18:09:27 +0300

    404 and common footer

--- src/_data/config.js
+++ src/_data/config.js
@@ -12,5 +12,37 @@ export default {
     facebook: "https://www.facebook.com/thecliffkallio/",
     instagram: "https://www.instagram.com/thecliffkallio/",
 
+  },
+
+  footer: {
+    contact: {
+      address: {
+        street: "Siltasaarenkatu 22",
+        postal: "00530 Helsinki, Finland",
+        mapsUrl: "https://maps.google.com/?q=Siltasaarenkatu+22,+00530+Helsinki,+Finland"
+      },
+      phone: "+358 505 387 662",
+      email: "info@thecliff.fi"
+    },
+    hours: [
+      { days: "Monday - Thursday", time: "10:00 - 24:00" },
+      { days: "Friday", time: "10:00 - 02:00" },
+      { days: "Saturday", time: "11:00 - 02:00" },
+      { days: "Sunday", time: "11:00 - 24:00" }
+    ],
+    kitchenCloses: "21:00",
+    social: [
+      { name: "Facebook", url: "https://www.facebook.com/thecliffkallio/", icon: "facebook" },
+      { name: "Instagram", url: "https://www.instagram.com/thecliffkallio/", icon: "instagram" }
+    ],
+    business: {
+      description: "Experience live music and craft cocktails in the most iconic venue in Kallio. Where music meets the night, and every evening tells a story.",
+      established: "2025",
+      capacity: "80 guests",
+      music: "Live performances at least weekly",
+      ageRestriction: "18+"
+    },
+    copyright: "© 2025 The Cliff. All rights reserved.",
+    designer: "muchmoredesign"
   }
 };
